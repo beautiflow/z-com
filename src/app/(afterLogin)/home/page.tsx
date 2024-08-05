bcbc1e74442b61e07d6229fa2286294727b56ecf
beautiflow <1114ny@naver.com>
2024-08-05 21:41:11 +0900

    section.02 PostForm 만들기

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,7 @@
 import style from './home.module.css';
 import Tab from '@/app/(afterLogin)/home/_component/Tab';
 import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
 
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
     <main className={style.main}>
       <TabProvider>
       <Tab />
-      {/* <PostForm />
-      <Post />
+      <PostForm />
+    {/*  <Post />
       <Post />
       <Post />
       <Post />
