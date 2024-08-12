06ebd1713a2788c140a8afa620786bb400dfea1f
beautiflow <1114ny@naver.com>
2024-08-12 21:14:41 +0900

    section.02 usePathname 과 /explore 페이지

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,8 +1,13 @@
-import style from './trendSection.module.css';
-import Trend from './Trend';
+"use client";
 
-export default function TrendSection(){
-    return(
+import style from './trendSection.module.css';
+import Trend from "@/app/(afterLogin)/_component/Trend";
+import { usePathname } from "next/navigation";
+
+export default function TrendSection() {
+    const pathname = usePathname();
+    if (pathname === '/explore') return null;
+    return (
         <div className={style.trendBg}>
             <div className={style.trend}>
                 <h3>나를 위한 트렌드</h3>
